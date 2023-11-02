1b41b604ca1cabd1c7c4ea79425de0936c090b71
German III <germaniiifelisarta@gmail.com>
2023-11-02 20:20:31 +0800

    Add Interactivity

--- simple-setup/setup-terminal.sh
+++ simple-setup/setup-terminal.sh
@@ -1,57 +1,122 @@
 !#/bin/bash
 
 ############################################################################
-# ARCH TERMINAL UTILITIES
+# DE SELECTION
 ############################################################################
 
-echo
-echo "Installing Terminal Utilities"
-echo
+# Provide a menu for the user to select the desktop environment
+echo "Select your desktop environment:"
+echo "1. GNOME"
+echo "2. Qtile"
+echo "3. Xfce desktop only"
+echo "4. Xfce with Goodies"
+echo "5. Hyprland"
+echo "6. Skip"
+echo "7. Quit"
 
-ARCHPKGS=(
-    'cronie'                  # cron jobs
-    'curl'                    # Remote content retrieval
-    'gtop'                    # System monitoring via terminal
-    'gufw'                    # Firewall manager
-    'hardinfo'                # Hardware info app
-    'bpytop'                  # Process viewer
-    'neofetch'                # Shows system info when you launch terminal
-    'ntp'                     # Network Time Protocol to set time via network.
-    'unrar'                   # RAR compression program
-    'unzip'                   # Zip compression program
-    'wget'                    # Remote content retrieval
-    'vim'                     # Terminal Editor
-    'zip'                     # Zip compression program
-    'ranger'    	      # Filesystem browser
-    'feh'  	              # Wallpaper changer
-    'neovim'
-)
+read -p "Enter the number of your choice: " choice
 
-for PKG in "${ARCHPKGS[@]}"; do
-    echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --noconfirm --needed
-done
+ARCHPKGS=()
 
-# Update font cache
-sudo fc-cache --force
+if [[ $choice == 1 ]]; then
+    ARCHPKGS+=('gnome')
+elif [[ $choice == 2 ]]; then
+    ARCHPKGS+=(
+        'qtile'
+        'rofi'
+        'flameshot'
+        'kitty'
+        'feh'
+        'dunst'
+        'copyq'
+        'picom'
+        'ufw'
+        'lightdm'
+        'arandr'
+        'pipewire'
+        'lib32-pipewire'
+        'helvum'
+    )
+elif [[ $choice == 3 ]]; then
+    ARCHPKGS+=(
+        'xfce4'
+        'ufw'
+    )
+elif [[ $choice == 4 ]]; then 
+    ARCHPKGS+=(
+        'xfce4'
+        'xfce4-goodies'
+    )
+elif [[ $choice == 5 ]]; then
+    ARCHPKGS+=(
+        'hyprland'
+        'lightdm'
+    )
+elif [[ $choice == 6 ]]; then
+    echo "Skipped Installing Desktop Environment"
+else
+    echo "Invalid choice. Please enter a valid number."
+    exit 1
+fi
 
-echo
-echo "Done!"
-echo
+############################################################################
+# ARCH TERMINAL UTILITIES
+############################################################################
+read -p "Would you like to install terminal utilities?[Y/n]" terminal_utilities_choice
+
+if [[ $terminal_utilities_choice == n ]]; then
+    echo "Skipping Terminal Utilities Setup"
+else
+    echo
+    echo "Installing Terminal Utilities"
+    echo
+
+    ARCHPKGS+=(
+        'cronie'                  # cron jobs
+        'curl'                    # Remote content retrieval
+        'gtop'                    # System monitoring via terminal
+        'gufw'                    # Firewall manager
+        'hardinfo'                # Hardware info app
+        'bpytop'                  # Process viewer
+        'neofetch'                # Shows system info when you launch terminal
+        'ntp'                     # Network Time Protocol to set time via network.
+        'unrar'                   # RAR compression program
+        'unzip'                   # Zip compression program
+        'wget'                    # Remote content retrieval
+        'vim'                     # Terminal Editor
+        'zip'                     # Zip compression program
+        'ranger'    	      # Filesystem browser
+        'feh'  	              # Wallpaper changer
+        'neovim'
+        'bpytop'
+    )
+
+    for PKG in "${ARCHPKGS[@]}"; do
+        echo "INSTALLING: ${PKG}"
+        sudo pacman -S "$PKG" --noconfirm --needed
+    done
+
+    # Update font cache
+    sudo fc-cache --force
+
+    echo
+    echo "Done!"
+    echo
+fi
 
 ############################################################################
 # AUR TERMINAL UTILITIES
 ############################################################################
-
+read -p "Would you like to install aur pakages?[Y/n]" aur_packs_choice
 
 AURPKGS=(
     # UTILITIES -----------------------------------------------------------
-
     'timeshift'                 # Backup and Restore
-    'helvum'
-    'zerotier-one'
+    # 'zerotier-one'
     'docker'
     'docker-compose'
+    'gruvbox-material-icon-theme-git'
+    'gruvbox-material-gtk-theme-git'
     'ttf-fira-code'
 )
 
@@ -66,8 +131,9 @@ echo
 echo "Done!"
 echo
 
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
+# NVIM Install with vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ############################################################################
 # NVIM SETUP
@@ -101,7 +167,4 @@ echo
 source ~/.bashrc
 nvm install --lts
 
-# NVIM Install with vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
