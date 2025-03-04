3a44908b61a1b2959d1f9bc4ccb4faed3335f2f6
germaniii <germaniiifelisarta@gmail.com>
2025-03-04 09:43:56 +0800

    [py-install] Add DE selection screen

--- curses-setup/constants/classes.py
+++ curses-setup/constants/classes.py
@@ -15,23 +15,26 @@ class DecoratedText(Enum):
     NORMAL = 1
     ALERT = 2
     WARNING = 3
-    SELECTED = 1
+    SELECTED = 4
+    DISABLED = 5
+    ACTIVE = 6
 
 
 class Screen(Enum):
-    MAIN_MENU = 'main_menu'
-    INSTALL_SELECT = 'install_select'
-    INSTALL_SUMMARY = 'install_summary'
-    INSTALL_CONFIRM = 'install_confirm'
-    INSTALL_COMPLETE = 'install_complete'
-    EXIT_CONFIRM = 'exit_confirm'
+    MAIN_MENU = "main_menu"
+    INSTALL_SELECT_DE = "install_select_de"
+    INSTALL_SELECT_PKGS = "install_select_pkgs"
+    INSTALL_SUMMARY = "install_summary"
+    INSTALL_CONFIRM = "install_confirm"
+    INSTALL_COMPLETE = "install_complete"
+    EXIT_CONFIRM = "exit_confirm"
 
 
 #####################################
 # CLASSES
 #####################################
 class DesktopEnvironment:
-    name = ""
+    name = DE.NONE
     description = ""
     packages = []
 
@@ -41,7 +44,7 @@ class DesktopEnvironment:
         self.packages = packages
 
     def __eq__(self, other):
-        return self.name == other.name
+        return self.name.value == other.name.value
 
 
 class Package:
--- curses-setup/screens/screen.py
+++ curses-setup/screens/screen.py
@@ -1,6 +1,6 @@
 import curses
 from typing import Tuple
-from constants.classes import DecoratedText
+from constants.colors import DecoratedText, get_color_pair
 
 
 class BaseScreen:
@@ -15,10 +15,9 @@ class BaseScreen:
         stdscr = self.stdscr
         key = stdscr.getch()
 
-        if key in (curses.KEY_UP, ord('k')) and current_row > 0:
+        if key in (curses.KEY_UP, ord("k")) and current_row > 0:
             current_row -= 1
-        elif (key in (curses.KEY_DOWN, ord('j'))
-              and current_row < len(self.items) - 1):
+        elif key in (curses.KEY_DOWN, ord("j")) and current_row < len(self.items) - 1:
             current_row += 1
         elif key in (curses.KEY_ENTER, 10, 13):
             current_row = 0
@@ -31,12 +30,12 @@ class BaseScreen:
         h, w = stdscr.getmaxyx()
 
         for idx, row in enumerate(self.items):
-            x = w//2 - len(row)//2
-            y = h//2 - len(self.items)//2 + idx
+            x = w // 2 - len(row) // 2
+            y = h // 2 - len(self.items) // 2 + idx
             if idx == current_row:
-                stdscr.attron(curses.color_pair(DecoratedText.SELECTED.value))
+                stdscr.attron(get_color_pair(DecoratedText.ACTIVE))
                 stdscr.addstr(y, x, row)
-                stdscr.attroff(curses.color_pair(DecoratedText.NORMAL.value))
+                stdscr.attron(get_color_pair(DecoratedText.NORMAL))
             else:
                 stdscr.addstr(y, x, row)
 
