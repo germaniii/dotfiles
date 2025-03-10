6b5506ecae0ec5841fc33554d18e45ed3369a0f1
germaniii <germaniiifelisarta@gmail.com>
2025-03-10 16:49:18 +0800

    [py-installer] redirect to final screen when finished installing

--- setup/screens/select_process.py
+++ setup/screens/select_process.py
@@ -1,4 +1,4 @@
-import curses
+import subprocess
 
 from constants.colors import get_color_pair
 from utils.install import install_package
@@ -17,41 +17,48 @@ class SelectProcessScreen(BaseScreen):
         self.error = False
 
     def watch_input(self, current_screen):
-        if self.success:
-            current_screen = Screen.EXIT_CONFIRM
-        elif self.error:
-            current_screen = Screen.MAIN_MENU
-
+        current_screen = Screen.INSTALL_COMPLETE
         return current_screen
 
     def print_menu(self) -> None:
         self.stdscr.clear()
         h, w = self.stdscr.getmaxyx()
 
-        try:
-            if not self.success or not self.error:
-                for index, package in enumerate(self.items):
-                    self.stdscr.clear()
-                    self.print_header(h, w, "INSTALLING", "")
-                    super().print_menu()
-                    self.stdscr.addstr(5, 0, "Installing package: " + package.name)
-                    for progress_unit in range(len(self.items)):
-                        if progress_unit <= index:
-                            self.stdscr.attron(get_color_pair(DecoratedText.WARNING))
+        if not self.success or not self.error:
+            for index, package in enumerate(self.items):
+                self.stdscr.clear()
+                self.print_header(h, w, "INSTALLING", "")
+                super().print_menu()
+                self.stdscr.addstr(5, 0, "Installing package: " + package.name)
+                for progress_unit in range(len(self.items)):
+                    if progress_unit <= index:
+                        # FIX: Not Working
+                        if self.items[progress_unit].name in [
+                            a.name
+                            for a in self.scrmanager.screens[
+                                Screen.INSTALL_COMPLETE
+                            ].error_items
+                        ]:
+                            self.stdscr.attron(get_color_pair(DecoratedText.ALERT))
                             self.stdscr.addstr(h // 2, progress_unit, " ")
                             self.stdscr.attron(get_color_pair(DecoratedText.NORMAL))
                         else:
-                            self.stdscr.attron(get_color_pair(DecoratedText.ACTIVE))
+                            self.stdscr.attron(get_color_pair(DecoratedText.WARNING))
                             self.stdscr.addstr(h // 2, progress_unit, " ")
                             self.stdscr.attron(get_color_pair(DecoratedText.NORMAL))
+                    else:
+                        self.stdscr.attron(get_color_pair(DecoratedText.ACTIVE))
+                        self.stdscr.addstr(h // 2, progress_unit, " ")
+                        self.stdscr.attron(get_color_pair(DecoratedText.NORMAL))
 
-                    self.stdscr.refresh()
+                self.stdscr.refresh()
+
+                try:
                     install_package(package)
+                except subprocess.CalledProcessError:
+                    self.scrmanager.append_error_items([package])
+            # rof
 
             self.success = True
-        except:
-            self.stdscr.addstr(5, 0, "There was a problem")
-            self.error = True
-            self.items = []
 
         self.stdscr.refresh()
