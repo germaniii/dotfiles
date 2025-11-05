6cb71b4b43870ff0a74b7077e0e653527cb84711
germaniii <germaniiifelisarta@gmail.com>
2025-11-05 13:40:40 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd ~
 curl -sS https://starship.rs/install.sh | sh
-git clone https://github.com/germaniii/dotfiles .
+git clone -b linux --single-branch https://github.com/germaniii/dotfiles .
 cd ./textual_setup/
 ./init.sh
